032363f95a69078d8d53b518e1129f0d0e62880d
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-11-30 13:08:32 -0300

    feat: implement uow and its generator

--- uow/uow.go
+++ uow/uow.go
@@ -5,12 +5,25 @@ import (
 	"database/sql"
 )
 
-type RepositoryFactory func(tx *sql.Tx) interface{}
+type Repository func(tx *sql.Tx) interface{}
 
 type UowInterface interface {
-	Register(name string, repositoryFactory RepositoryFactory)
+	Register(name string, repository Repository)
 	Unregister(name string)
 	GetRepository(ctx context.Context, name string) (interface{}, error)
 	Do(ctx context.Context, fn func(uow UowInterface) error) error
 	CommitOrRollback() error
+}
+
+type UnitOfWork struct {
+	DB           *sql.DB
+	Tx           *sql.Tx
+	Repositories map[string]Repository
+}
+
+func NewUnitOfWork(db *sql.DB) *UnitOfWork {
+	return &UnitOfWork{
+		DB:           db,
+		Repositories: make(map[string]Repository),
+	}
 }
